1b7921e3ac8a784a05ab4aaca00fd0a38dd14dd9
Chinoms <chinoms925@gmail.com>
2019-04-06 18:44:26 +0100

    Update login.php

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
     <body class="hold-transition login-page">
         <div class="login-box">
             <div class="login-logo">
-                <a href="../../index2.html"><h2>CEERS</h2></a>
+                <a href="#"><h2>CEERS</h2></a>
             </div>
             <!-- /.login-logo -->
             <div class="login-box-body">
